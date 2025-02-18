640cf659341339cf337aa0f6b9796fda112a0c9e
Cengizhan Ozeyranoglu <cengizhan.ozeyran@hotmail.com>
2025-02-18 18:44:34 +0300

    added company microservice

---
+++ companymicroservices/src/main/java/com/cengizhanozeyranoglu/companymicroservices/CompanymicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.cengizhanozeyranoglu.companymicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanymicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanymicroservicesApplication.class, args);
+	}
+
+}
